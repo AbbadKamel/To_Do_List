50b08294252c003b2d36c79688ee88825f3c5dee
LahceneBousadia <lahcene.bousadia@gmail.com>
2021-03-21 04:59:14 +0100

    LAHCENE BOUSADIA Adding of the API folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
+import authTokenConfig from './auth';
 
+const API_SERVER_IP = "138.68.74.39";
+axios.defaults.baseURL = `http://${API_SERVER_IP}/api/`;
+axios.interceptors.request.use(config => {
+    config = authTokenConfig(config);
+    return config;
+});
+export default axios;
