7ac096cd7ae547622ec13c3ad35ee05d08f3fc67
hamza abouhammadi <ab.hamza1997@gmail.com>
2021-03-22 22:32:27 +0100

    Signed-off-by: hamza abouhammadi <ab.hamza1997@gmail.com>
    We have created register page where the user can create a new account with error handling like mail already existed or password must be at least 6 characters

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,98 @@
 <template>
- </template>
+  <main>
+    <h1 style="color: #758241">SIGN UP</h1>
+    <form class="center" @submit.prevent="adduser()">
+      <div class="log-reg">
+        <input
+          v-model="name"
+          type="text"
+          name="name"
+          placeholder="FULL Name"
+          required
+        />
+      </div>
+      <div class="log-reg">
+        <input
+          v-model="email"
+          type="email"
+          name="email"
+          placeholder="EMAIL"
+          required
+        />
+      </div>
+      <div class="log-reg">
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+          placeholder="PASSWORD"
+          required
+        />
+      </div>
+      <div class="log-reg">
+        <input
+          v-model="confirmPassword"
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme Password"
+          required
+        />
+      </div>
+      <div v-if="getErrorMessage !== ''" class="messageerror">
+        {{ getErrorMessage }}
+      </div>
+      <div v-if="message !== ''" class="messageerror">
+        {{ message }}
+      </div>
+
+      <button type="submit" class="Valider_btn">SIGN UP</button>
+
+      <router-link :to="{ name: 'home' }">
+        <button class="Valider_btn2">SIGN IN</button>
+      </router-link>
+    </form>
+  </main>
+</template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 
+export default {
+  name: "Register",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      message: "",
+    };
+  },
+  methods: {
+    ...mapActions("account", ["register"]),
+    adduser() {
+      if (this.password === this.confirmPassword) {
+        this.register({
+          name: this.name,
+          email: this.email,
+          password: this.password,
+        });
+      } else {
+        this.message = "Incorrect Password";
+      }
+    },
+  },
+  computed: {
+    ...mapGetters("account", ["isLogIn", "getErrorMessage"]),
+  },
+  mounted() {
+    if (this.isLogIn) {
+      this.$router.replace("/");
+    }
+  },
+};
 </script>
 
 <style scoped>
-
+@import "../components/app.css";
 </style>
