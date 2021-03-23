fe417fcfc7e6557842ca4d061b1a90a9f37123bd
unknown <kameloubrahimabbad19@gmail.com>
2021-03-24 00:05:21 +0100

    Signed-off-by: unknown <kameloubrahimabbad19@gmail.com>
    here we have make the home page and navbar where we have our menu which is responsive and can change his size when user in smartphone

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,60 @@
 <template>
 
+<header class="header">
+
+    <router-link :to="{name: 'home'}">
+      <img src="../assets/todo.png" alt="Logo Todo" id="our_logo">
+    </router-link>
+
+    <input 
+      type="checkbox" 
+      id="menu-btn" 
+      class="menu-btn" 
+    />
+    <label 
+      for="menu-btn"
+      class="menu-icon" 
+    > <span 
+        class="navicon"
+      >
+      </span>
+    </label>
+
+    <ul class="menu">
+
+      <li>
+        <router-link  
+          :to="{name: 'about'}" 
+          class="btn_menu"
+        > About us 
+        </router-link>
+      </li>
+      <li>
+        <a v-if="isLogIn"  href="" @click="logout()" style="color:'red'">Log Out</a>
+        <router-link v-else  :to="{name: 'login'}" class="btn_menu">Log in</router-link>
+      </li>
+    </ul>
+</header>
 
 
 
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Navbar',
+  methods: {
+    ...mapActions("account", ["getAccountData", "logout"]),
+
+  },
+  computed: {
+    ...mapGetters("account", ['isLogIn']),
+  },
+}
 </script>
 
 <style scoped>
+@import './navbar.css';
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,54 @@
 <template>
-    
+  <div id="root">
+    <sidebar :allTodoList="getTodolistData" :method="setList" />
+    <main>
+      <todo-list :todolist="list" />
+    </main>
+  </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList";
+import Sidebar from "@/components/Sidebar";
+
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "Home",
+  components: {
+    TodoList,
+    Sidebar,
+  },
+  data() {
+    return {
+      list: null,
+    };
+  },
+  methods: {
+    affiche: function () {
+      console.log(this.list);
+    },
+    setList: function (todolist) {
+      this.list = todolist;
+    },
+    ...mapActions("todolist", ["getTodolists"]),
+  },
+  computed: {
+    ...mapGetters("todolist", ["getTodolistData"]),
+  },
+  mounted() {
+    this.getTodolists();
+  },
+};
 </script>
 
 <style scoped>
+/* CHANGEEEEEEEEEEEEEEE ICI*****************/
+#root {
+  display: grid;
+  grid-template-columns: 25% 75%;
+  min-height: calc(100vh - 55px);
+}
 
+@import "../components/app.css";
 </style>
